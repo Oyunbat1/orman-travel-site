80284297f0ed23396f903a43078d87275c954970
Oyunbat <oyunbat9958@gmail.com>
2024-01-16 15:52:54 +0800

    reveal on scroll hiij dusaw ashgui

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,5 +1,10 @@
 import "../styles/styles.css";
 import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
+
+new RevealOnScroll(document.querySelectorAll(".feature-items"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonials"), 50);
+
 let MobileMenuIn = new MobileMenu();
 
 if (module.hot) {
