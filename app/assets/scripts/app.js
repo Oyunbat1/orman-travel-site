5abe10ae11a314ab1dbb8dc6b9d30c2c98b1dee3
Oyunbat <oyunbat9958@gmail.com>
2024-01-16 15:56:26 +0800

    app.js file deer shine huwisagch zarlaj uguw

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -4,7 +4,7 @@ import RevealOnScroll from "./modules/RevealOnScroll";
 
 new RevealOnScroll(document.querySelectorAll(".feature-items"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonials"), 50);
-
+let RevealOnScrollIn = new RevealOnScroll();
 let MobileMenuIn = new MobileMenu();
 
 if (module.hot) {
