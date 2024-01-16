b8da2bfa4c5100365667d7b1eb5a0281460ac9a2
Oyunbat <oyunbat9958@gmail.com>
2024-01-16 13:57:09 +0800

    fix hilee

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,6 +1,6 @@
 import "../styles/styles.css";
 import MobileMenu from "./modules/MobileMenu";
-let mobileMenu = new MobileMenu();
+let MobileMenuIn = new MobileMenu();
 
 if (module.hot) {
   module.hot.accept();
