41437f1c010284c548cb06f372ba6620636d18fa
Oyunbat <oyunbat9958@gmail.com>
2024-01-17 21:00:22 +0800

    sticky header dussan gehde color soligdohgui baina

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,10 +1,17 @@
 import "../styles/styles.css";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
+import StickyHeader from "./modules/StickyHeader";
 
-new RevealOnScroll(document.querySelectorAll(".feature-items"), 75);
+let StickyHeaderIn = new StickyHeader();
+
+let RevealOnScrollIn = new RevealOnScroll(
+  document.querySelectorAll(".feature-items"),
+  75
+);
+
 new RevealOnScroll(document.querySelectorAll(".testimonials"), 50);
-let RevealOnScrollIn = new RevealOnScroll();
+
 let MobileMenuIn = new MobileMenu();
 
 if (module.hot) {
