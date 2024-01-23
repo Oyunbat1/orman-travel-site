fc62a0105e697523c79de3cea4939a9f8fd158a3
Oyunbat <oyunbat9958@gmail.com>
2024-01-23 14:05:10 +0800

    cloud functionig update hiij hereglegcin nuts ug newtreh bolomjtoi bolow

--- cloud-functions/secret-pages.js
+++ cloud-functions/secret-pages.js
@@ -1,6 +1,24 @@
 exports.handler = function (event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Welcome to our acc.",
-  });
+  const secretContent = `
+<h3> Welcome to our acc.Залуу минь азтай байна шүү чи бас!! </h3>
+<p> Зөвхөн чамд зориулав </p>
+`;
+  let body;
+
+  if (event.body) {
+    body = JSON.parse(event.body);
+  } else {
+    body = {};
+  }
+
+  if (body.password == "JavaScript") {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent,
+    });
+  } else {
+    callback(null, {
+      statusCode: 401,
+    });
+  }
 };
