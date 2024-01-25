578579d39de7093590e67fbfaac71125b9796fec
Oyunbat <oyunbat9958@gmail.com>
2024-01-25 19:44:08 +0800

    cloud function update hiiw

--- cloud-funct/cloud-function.js
+++ cloud-funct/cloud-function.js
@@ -1,6 +1,23 @@
 exports.handler = function (event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "welcome to our page ",
-  });
+  const secretContent = `
+    <h3> This is privite web page welcome <h3>
+    <p>Youre so  <strong> lucky </strong> guy</p>
+`;
+  let body;
+
+  if (event.body) {
+    body = JSON.parse(event.body);
+  } else {
+    body = {};
+  }
+  if (body.password == "JavaScript") {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent,
+    });
+  } else {
+    callback(null, {
+      statusCode: 401,
+    });
+  }
 };
