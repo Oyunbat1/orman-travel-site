fb8cc2561c5c831913ceddc641670089878d9ead
Oyunbat <oyunbat9958@gmail.com>
2024-01-25 18:42:10 +0800

    cloud-functions nemle

--- app/cloud-functions/cloud-functions.js
+++ app/cloud-functions/cloud-functions.js
@@ -1,6 +1,6 @@
 exports.handler = function (event, context, callback) {
   callback(null, {
     statusCode: 200,
-    body: "welcome to our page , it is only yours",
+    body: "welcome to our page , it is only your web browser",
   });
 };
